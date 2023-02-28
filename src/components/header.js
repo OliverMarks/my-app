85bf1f31565b74cb5efe92f39f4dc3665d13c054
oilythighs <oly.marks@gmail.com>
2023-02-28 18:03:58 +1000

    title

--- src/components/header.js
+++ src/components/header.js
@@ -6,8 +6,8 @@ export default function Header (props) {
 
     return (
         <div className="App-header">
-        <h1>Album Art Canvas Creator
-        </h1>
+            
+        <h1>Album Art Canvas Creator</h1>
         </div>
     )
 
